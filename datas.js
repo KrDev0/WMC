97617cd28d939d9879ee19858f7955dd0a4f8455
KrDev <78448522+KrDev0@users.noreply.github.com>
2024-04-03 23:21:18 -0500

    Add files via upload

--- datas.js
+++ datas.js
@@ -1,7 +1,52 @@
 [
-  {
-    "Edition": "Windows 10 Pro 21h2",
-    "Size": "3.24 GB",
-    "Server": "Enlace de descarga"
-  }
+    {
+        "Edition": "Windows 10 Pro 21h2 (x86)",
+        "Size": "3.24 GB",
+        "Server": "GitHub"
+    },
+    {
+        "Edition": "Windows 10 Pro 21h2 (x64)",
+        "Size": "3.24 GB",
+        "Server": "GitHub"
+    },
+    {
+        "Edition": "Windows 10 Home 21h2 (x86)",
+        "Size": "3.10 GB",
+        "Server": "GitHub"
+    },
+    {
+        "Edition": "Windows 10 Home 21h2 (x64)",
+        "Size": "3.10 GB",
+        "Server": "GitHub"
+    },
+    {
+        "Edition": "Windows 10 Enterprice 21h2 (x86)",
+        "Size": "3.50 GB",
+        "Server": "GitHub"
+    },
+    {
+        "Edition": "Windows 10 Enterprice 21h2 (x64)",
+        "Size": "3.50 GB",
+        "Server": "GitHub"
+    },
+    {
+        "Edition": "Windows 10 Pro N 21h2 (x86)",
+        "Size": "3.05 GB",
+        "Server": "GitHub"
+    },
+    {
+        "Edition": "Windows 10 Pro N 21h2 (x64)",
+        "Size": "3.05 GB",
+        "Server": "GitHub"
+    },
+    {
+        "Edition": "Windows 10 Home N 21h2 (x86)",
+        "Size": "3.00 GB",
+        "Server": "GitHub"
+    },
+    {
+        "Edition": "Windows 10 Home N 21h2 (x64)",
+        "Size": "3.00 GB",
+        "Server": "GitHub"
+    }
 ]
