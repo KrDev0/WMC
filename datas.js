7bf594cc8e39b0a01bfec37cc8b95c09a1ca03ad
KrDev <78448522+KrDev0@users.noreply.github.com>
2024-04-05 21:01:54 -0500

    Update datas.js

--- datas.js
+++ datas.js
@@ -1,11 +1,56 @@
 [
     {
-        "Edition": "MiniOS 11 Pro",
-        "Windows": "Windows 11 23H2",
+        "Edition": "WinterOS",
+        "Windows": "Windows 10 Pro 22H2",
         "Arch": "x64",
-        "Version": "v2024.02",
-        "Develooper": "Doofy's",
-        "Size": "3.24 GB",
-        "Server": "https://github.com/KrDev0/HBiblia/raw/main/BJPE"
+        "Version": "Rev11",
+        "Develooper": "Mauricio Cerqueiro",
+        "Size": "3.45 GB",
+        "Server": "https://download2388.mediafire.com/8x7h10gvb4ngPHWHxCqzcC3QRXa50B_XpVNugSZCM89KVHEhOWVNhM9g3_dEmW5cpe2UIfz8CQUNm-nxxxidGd2sxgBXKnulFqlSoq5THYz3Vq1s-gDWvnOtzygCKGQhB6ssdTixAk1hndYMuVJzwijgvFGftOET_8nQWN-RBKkpkpAoWg/cqax5bqectgdqpi/22H2x64W10Rev11.iso"
+    },
+    {
+        "Edition": "WinterOS",
+        "Windows": "Windows 10 Pro 22H2",
+        "Arch": "x32",
+        "Version": "Rev11",
+        "Develooper": "Mauricio Cerqueiro",
+        "Size": "2.54 GB",
+        "Server": "https://download2300.mediafire.com/gdnxorgcayugdPb_w0Xy6JnanIBNH_Mpkb2pO7FA050j5zeDjOcOvNAhVGHyBdHZPeNaCPJK0lmBzyg-MxNfWmg8nVwp9hRY6wbL_l5-5QbZvmo-0pFYqhn81j4Jc95c1YkdKyRczHCGpWrfQqCgehdRJSiS2sSVVBnYCndTfUfz85S6ow/mcszudmq7aap5sr/22H2x32W10Rev11.iso"
+    },
+    {
+        "Edition": "WinterOS",
+        "Windows": "Windows 10 LTSC2021",
+        "Arch": "x64",
+        "Version": "Rev11",
+        "Develooper": "Mauricio Cerqueiro",
+        "Size": "3.8 GB",
+        "Server": "https://download2279.mediafire.com/7tj4yd1fi3bgSQy4bT2dJC2bVxXVjxxXvvWPrLyfNOpDw-SqBzCwI39rzIVOMOr55ybVd_jnouzjAo8VI_LZwT_ENzfgJKCAClcLVRDp6V3pMJPdRhrIZWA2_LBOYuwD6XObVU1EyRwadjpwdp7gA0zmqL_THSOMwSOTCMSwqqeVndNVgw/5fs8o58xsme3gji/LTSC2021x64Rev11.iso"
+    },
+    {
+        "Edition": "WinterOS",
+        "Windows": "Windows 10 LTSC2021",
+        "Arch": "x32",
+        "Version": "Rev11",
+        "Develooper": "Mauricio Cerqueiro",
+        "Size": "2.8 GB",
+        "Server": "https://download2446.mediafire.com/mgwfy9gjmeyggEagsNZnnDAxgaGUHLftHjyoR8KBmPdgLfCTBdotUU0xL5odIY1J8F9EXqN23swuqbFZ3WId8QYTsdfEXMmpOooO1fWUY2h0Y54F-o-tEvHnfl5jDJZd82FTyOhqq8Co8ppDktIdQYsvSvu85K5R67swg8HOJSfLqnFSAQ/at5whpf30on4ybm/LTSC2021x32Rev11.iso"
+    },
+    {
+        "Edition": "WinterOS",
+        "Windows": "Windows 10 LTSC2019",
+        "Arch": "x64",
+        "Version": "Rev11",
+        "Develooper": "Mauricio Cerqueiro",
+        "Size": "3.61 GB",
+        "Server": "https://download2390.mediafire.com/r78wjrl3b07g8kmNnAyly8hAVtYiuxEEmTjuMsw4lIuhLNqHDjCT_XPG90N1Lsbvf9sJUyxbrXV79G4RhmrTcLiDcnkwnfWfKeCEd_Kb_O9VjHAy1tg5UTx_6SgpK7Dz5vHyvlVaolnt0-CNWwqHAJ-SDsD34oy5W2g-Sr_8D7_q2_QXdQ/ocmjhe2lt9xl0ov/LTSC2019x64Rev11.iso"
+    },
+    {
+        "Edition": "WinterOS",
+        "Windows": "Windows 10 LTSC2019",
+        "Arch": "x32",
+        "Version": "Rev11",
+        "Develooper": "Mauricio Cerqueiro",
+        "Size": "2.62 GB",
+        "Server": "https://download940.mediafire.com/09gh65ywlvugrfmQi-TqNIX7K8ws6UcBvZxjD7t2e4IIuiW_F-DA1tA5nnafA27MM_UaDTBrh8q3nSM4ZJUecw75nB8t5Nuu1G6CLFUuE0JLKFlGVgRT9g4lN8IT7zD7TH07jFJ3fg7TLGOdN-52fQjrzt_yVniZeVUz2F52KhqtqSH34g/wz63xovrx5navew/LTSC2019x32Rev11.iso"
     }
 ]
